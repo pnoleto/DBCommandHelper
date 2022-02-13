4365a29a705742469177bd39bd3ac5a00da16e82
PhelipeNoleto <phelipe.noleto@outlook.com>
2022-02-13 17:19:59 -0300

    Extension Methods implementation

--- DBCommandHelper/DBCommandHelper.cs
+++ DBCommandHelper/DBCommandHelper.cs
@@ -4,113 +4,124 @@ using System.Data;
 
 namespace Domain.Helpers.Infrastructure
 {
-    public class DBCommandHelper
+    public static class DBCommandHelper
     {
+        private readonly static IDictionary<Type, DbType> typeMap = new Dictionary<Type, DbType>
+        {
+            [typeof(byte)] = DbType.Byte,
+            [typeof(byte?)] = DbType.Byte,
+            [typeof(sbyte?)] = DbType.SByte,
+            [typeof(sbyte)] = DbType.SByte,
+            [typeof(byte[])] = DbType.Binary,
+            [typeof(short)] = DbType.Int16,
+            [typeof(short?)] = DbType.Int16,
+            [typeof(ushort)] = DbType.UInt16,
+            [typeof(ushort?)] = DbType.UInt16,
+            [typeof(int)] = DbType.Int32,
+            [typeof(int?)] = DbType.Int32,
+            [typeof(uint)] = DbType.UInt32,
+            [typeof(uint?)] = DbType.UInt32,
+            [typeof(long)] = DbType.Int64,
+            [typeof(long?)] = DbType.Int64,
+            [typeof(ulong)] = DbType.UInt64,
+            [typeof(ulong?)] = DbType.UInt64,
+            [typeof(float)] = DbType.Single,
+            [typeof(float?)] = DbType.Single,
+            [typeof(double)] = DbType.Double,
+            [typeof(double?)] = DbType.Double,
+            [typeof(decimal)] = DbType.Decimal,
+            [typeof(decimal?)] = DbType.Decimal,
+            [typeof(bool)] = DbType.Boolean,
+            [typeof(bool?)] = DbType.Boolean,
+            [typeof(string)] = DbType.String,
+            [typeof(char)] = DbType.StringFixedLength,
+            [typeof(char?)] = DbType.StringFixedLength,
+            [typeof(Guid)] = DbType.Guid,
+            [typeof(Guid?)] = DbType.Guid,
+            [typeof(DateTime)] = DbType.DateTime,
+            [typeof(DateTime?)] = DbType.DateTime,
+            [typeof(DateTimeOffset)] = DbType.DateTimeOffset,
+            [typeof(DateTimeOffset?)] = DbType.DateTimeOffset
+            //typeMap[typeof(Binary)] = DbType.Binary;
+        };
+
+        private static void IfDbCommandNullThrowException(this IDbCommand dbCommand)
+        {
+            if (dbCommand == null) throw new ArgumentNullException(nameof(IDbCommand));
+        }
         /// <summary>
         /// Dicionario utilizado para resolver o DbType de um determinado tipo primitivo
         /// </summary>
         /// <param name="type">tipo do objeto que sera resolvido para DbType</param>
+        /// TODO: How to map bynary type
         /// <returns></returns>
-        private static DbType ObterTipoDoParametro(Type type)
+        private static DbType ReturnParameterType(Type type)
         {
-            var typeMap = new Dictionary<Type, DbType>();
-            typeMap[typeof(byte)] = DbType.Byte;
-            typeMap[typeof(sbyte)] = DbType.SByte;
-            typeMap[typeof(short)] = DbType.Int16;
-            typeMap[typeof(ushort)] = DbType.UInt16;
-            typeMap[typeof(int)] = DbType.Int32;
-            typeMap[typeof(uint)] = DbType.UInt32;
-            typeMap[typeof(long)] = DbType.Int64;
-            typeMap[typeof(ulong)] = DbType.UInt64;
-            typeMap[typeof(float)] = DbType.Single;
-            typeMap[typeof(double)] = DbType.Double;
-            typeMap[typeof(decimal)] = DbType.Decimal;
-            typeMap[typeof(bool)] = DbType.Boolean;
-            typeMap[typeof(string)] = DbType.String;
-            typeMap[typeof(char)] = DbType.StringFixedLength;
-            typeMap[typeof(Guid)] = DbType.Guid;
-            typeMap[typeof(DateTime)] = DbType.DateTime;
-            typeMap[typeof(DateTimeOffset)] = DbType.DateTimeOffset;
-            typeMap[typeof(byte[])] = DbType.Binary;
-            typeMap[typeof(byte?)] = DbType.Byte;
-            typeMap[typeof(sbyte?)] = DbType.SByte;
-            typeMap[typeof(short?)] = DbType.Int16;
-            typeMap[typeof(ushort?)] = DbType.UInt16;
-            typeMap[typeof(int?)] = DbType.Int32;
-            typeMap[typeof(uint?)] = DbType.UInt32;
-            typeMap[typeof(long?)] = DbType.Int64;
-            typeMap[typeof(ulong?)] = DbType.UInt64;
-            typeMap[typeof(float?)] = DbType.Single;
-            typeMap[typeof(double?)] = DbType.Double;
-            typeMap[typeof(decimal?)] = DbType.Decimal;
-            typeMap[typeof(bool?)] = DbType.Boolean;
-            typeMap[typeof(char?)] = DbType.StringFixedLength;
-            typeMap[typeof(Guid?)] = DbType.Guid;
-            typeMap[typeof(DateTime?)] = DbType.DateTime;
-            typeMap[typeof(DateTimeOffset?)] = DbType.DateTimeOffset;
-            //typeMap[typeof(Binary)] = DbType.Binary;
             return typeMap[type];
         }
         /// <summary>
         /// Cria um parametro no IDbCommand informado
         /// </summary>
-        /// <param name="DbCommand">IDbCommand que sera adicionado o parametro</param>
-        /// <param name="NomeParametro">Nome do parametro que sera criado no IDbCommand</param>
-        /// <param name="TipoParametro">Tipo de dados DbType que sera passado no parametro</param>
-        public static void CriarParametro(IDbCommand DbCommand, string NomeParametro, DbType TipoParametro)
+        /// <param name="dbCommand">IDbCommand que sera adicionado o parametro</param>
+        /// <param name="paramName">Nome do parametro que sera criado no IDbCommand</param>
+        /// <param name="paramType">Tipo de dados DbType que sera passado no parametro</param>
+        public static void CreateParamWithName(this IDbCommand dbCommand, string paramName, DbType paramType)
         {
-            IDataParameter param = DbCommand.CreateParameter();
-            param.ParameterName = NomeParametro;
-            param.DbType = TipoParametro;
+            dbCommand.IfDbCommandNullThrowException();
 
-            DbCommand.Parameters.Add(param);
+            IDataParameter param = dbCommand.CreateParameter();
+            param.ParameterName = paramName;
+            param.DbType = paramType;
+
+            dbCommand.Parameters.Add(param);
         }
 
         /// <summary>
         /// Adiciona valor ao parametro que foi criado anteriormente
         /// </summary>
         /// <typeparam name="TTipo">Tipo do objeto que sera o valor do parametro</typeparam>
-        /// <param name="DbCommand">IDbCommand que sera adicionado o parametro</param>
-        /// <param name="NomeParametro">Nome do parametro que sera criado no IDbCommand</param>
-        /// <param name="Valor">Valor que sera adicionado ao parametro</param>
-        public static void AdicionarValorAoParametro<TTipo>(IDbCommand DbCommand, string NomeParametro, TTipo Valor)
+        /// <param name="dbCommand">IDbCommand que sera adicionado o parametro</param>
+        /// <param name="paramName">Nome do parametro que sera criado no IDbCommand</param>
+        /// <param name="paramValue">Valor que sera adicionado ao parametro</param>
+        public static void SetParamValue<TTipo>(this IDbCommand dbCommand, string paramName, TTipo paramValue)
         {
-            ((IDataParameter)DbCommand.Parameters[NomeParametro]).Value = SeNuloARetornaDbNull(Valor);
+            dbCommand.IfDbCommandNullThrowException();
+
+            ((IDataParameter)dbCommand.Parameters[paramName]).Value = IfNullThenReturnDbNullValue(paramValue);
         }
         /// <summary>
         /// Cria um parametro no IDbCommand informado com um determinado DbType nome e valor
         /// </summary>
         /// <typeparam name="TTipo">Tipo do objeto que sera o valor do parametro</typeparam>
-        /// <param name="DbCommand">IDbCommand que sera adicionado o parametro</param>
-        /// <param name="NomeParametro">Nome do parametro que sera criado no IDbCommand</param>
-        /// <param name="TipoParametro">Tipo de dados DbType que sera passado no parametro</param>
-        /// <param name="Valor">Valor que sera adicionado ao parametro</param>
-        public static void CriarParametroComValor<TTipo>(IDbCommand DbCommand, string NomeParametro, DbType TipoParametro, TTipo Valor)
+        /// <param name="dbCommand">IDbCommand que sera adicionado o parametro</param>
+        /// <param name="paramName">Nome do parametro que sera criado no IDbCommand</param>
+        /// <param name="paramType">Tipo de dados DbType que sera passado no parametro</param>
+        /// <param name="paramValue">Valor que sera adicionado ao parametro</param>
+        public static void CreateParamWithNameTypeAndValue<TTipo>(this IDbCommand dbCommand, string paramName, DbType paramType, TTipo paramValue)
         {
-            CriarParametro(DbCommand, NomeParametro, TipoParametro);
-            AdicionarValorAoParametro(DbCommand, NomeParametro, SeNuloARetornaDbNull(Valor));
+            dbCommand.IfDbCommandNullThrowException();
+
+            CreateParamWithName(dbCommand, paramName, paramType);
+            SetParamValue(dbCommand, paramName, IfNullThenReturnDbNullValue(paramValue));
         }
 
         /// <summary>
         /// Criar um parametro com valor no IBbCommand sem a necessidade de definir o DbType
         /// </summary>
-        /// <param name="DbCommand">IDbCommand que sera adicionado o parametro com valor</param>
-        /// <param name="NomeParametro">Nome do parametro que sera criado no IDbCommand</param>
-        /// <param name="Valor">Valor que sera adicionado ao parametro</param>
-        public static void CriarParametroComValor(IDbCommand DbCommand, string NomeParametro, object Valor)
+        /// <param name="dbCommand">IDbCommand que sera adicionado o parametro com valor</param>
+        /// <param name="paramName">Nome do parametro que sera criado no IDbCommand</param>
+        /// <param name="paramValue">Valor que sera adicionado ao parametro</param>
+        public static void CreateParamWithNameAndValue(this IDbCommand dbCommand, string paramName, object paramValue)
         {
-                CriarParametro(DbCommand, NomeParametro, ObterTipoDoParametro(Valor.GetType()));
-                AdicionarValorAoParametro(DbCommand, NomeParametro, SeNuloARetornaDbNull(Valor));
+            dbCommand.IfDbCommandNullThrowException();
+
+            CreateParamWithName(dbCommand, paramName, ReturnParameterType(paramValue.GetType()));
+            SetParamValue(dbCommand, paramName, IfNullThenReturnDbNullValue(paramValue));
         }
 
-        private static object SeNuloARetornaDbNull(object Valor)
+        private static object IfNullThenReturnDbNullValue(object inputValue)
         {
-            object valor = DBNull.Value;
-
-            if (!Equals(valor, null))
-                valor = Valor;
-
-            return valor;
+            return inputValue ?? DBNull.Value;
         }
     }
 }
