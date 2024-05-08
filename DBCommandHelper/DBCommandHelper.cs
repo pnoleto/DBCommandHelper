f32c0380b5cd2601fa703d8c7bec259cf1d81d43
Phelipe Noleto <48934827+pnoleto@users.noreply.github.com>
2024-05-07 21:47:58 -0300

    feat: version update

--- DBCommandHelper/DBCommandHelper.cs
+++ DBCommandHelper/DBCommandHelper.cs
@@ -44,18 +44,6 @@ namespace DBCommandHelper
             [typeof(DateTimeOffset?)] = DbType.DateTimeOffset
         };
 
-        private static void IfDbCommandNullThrowException(this IDbCommand dbCommand)
-        {
-            if (dbCommand == null) throw new ArgumentNullException(nameof(dbCommand));
-        }
-
-        private static void IfParamNameNullThrowException(this string paramName)
-        {
-            if (paramName == null) throw new ArgumentNullException(nameof(paramName));
-
-            if (string.IsNullOrEmpty(paramName.Trim())) throw new ArgumentNullException(nameof(paramName));
-        }
-
         private static void IfParamTypeNullThrowException(Type type)
         {
             if (type == null) throw new ArgumentNullException(nameof(type));
@@ -91,46 +79,6 @@ namespace DBCommandHelper
             dbCommand.SetParamValue(parameterName, IfNullReturnDbNullValue(parameterValue));
         }
 
-        [Obsolete("This method is obsolete, use dbCommand.CreateParam('paramName', 'paramValue', DbType.VarChar) instead")]
-        public static void CreateParamWithNameAndType(this IDbCommand dbCommand, string paramName, DbType paramType)
-        {
-            dbCommand.IfDbCommandNullThrowException();
-            paramName.IfParamNameNullThrowException();
-
-            IDataParameter param = dbCommand.CreateParameter();
-
-            param.ParameterName = paramName;
-            param.DbType = paramType;
-
-            dbCommand.Parameters.Add(param);
-        }
-
-        [Obsolete("This method is obsolete, use dbCommand.CreateParam('paramName', 'paramValue', 'value') instead")]
-        public static void CreateParamWithNameTypeAndValue<TType>(this IDbCommand dbCommand, string paramName, DbType paramType, TType paramValue)
-        {
-            dbCommand.IfDbCommandNullThrowException();
-            paramName.IfParamNameNullThrowException();
-
-            dbCommand.CreateParamWithNameAndType(paramName, paramType);
-            dbCommand.SetParamValue(paramName, IfNullReturnDbNullValue(paramValue));
-        }
-
-        [Obsolete("This method is obsolete, use dbCommand.CreateParam('paramName', 'paramValue') instead")]
-        public static void CreateParamWithNameAndValue(this IDbCommand dbCommand, string paramName, object paramValue)
-        {
-            dbCommand.IfDbCommandNullThrowException();
-            paramName.IfParamNameNullThrowException();
-
-            dbCommand.CreateParamWithNameAndType(paramName, ReturnParameterType(paramValue.GetType()));
-            dbCommand.SetParamValue(paramName, IfNullReturnDbNullValue(paramValue));
-        }
-
-        [Obsolete("This method is obsolete")]
-        public static void ClearParamValue(this IDbCommand dbCommand, string paramName)
-        {
-            dbCommand.SetParamValue(paramName, DBNull.Value);
-        }
-
         public static void SetParamValue<TType>(this IDbCommand dbCommand, string paramName, TType paramValue)
         {
             ((IDataParameter)dbCommand.Parameters[paramName]).Value = IfNullReturnDbNullValue(paramValue);
@@ -139,12 +87,6 @@ namespace DBCommandHelper
         public static void ClearValues(this IDataParameterCollection parametersConnection)
         {
             foreach (IDataParameter param in parametersConnection) param.Value = DBNull.Value;
-        }
-
-        [Obsolete("This method is obsolete, use dbCommand.parameters.Clear instead")]
-        public static void CleanAllParamValues(this IDbCommand dbCommand)
-        {
-            for (int i = 0; i < dbCommand.Parameters.Count; i++) ((IDataParameter)dbCommand.Parameters[i]).Value = DBNull.Value;
         }
 
         private static object IfNullReturnDbNullValue(object inputValue)
